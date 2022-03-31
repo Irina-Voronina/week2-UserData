99fe6cbe8dcfd2ceb182b2274f05e0f0f92ebc02
Irina Voronina <irinaworonina@gmail.com>
2022-03-31 22:55:55 +0300

    add my second c# project

---
+++ DrivingLicense/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DrivingLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //приложение просит пользователя ввести его год рождения
+            //приложение вычисляет возраст пользователя (2022 - год рождения пользователя)
+            //если пользователь моложе 18 лет, то в консоли выводится сколько лет и "too young to drive"
+            //если пользователь старше 18 лет, то в консоли выводится сколько лет и "drive carefully"
+            //если пользователю 18 лет, то в консоли выводится сколько лет и "Congratulations, you may apply
+            //for your driving license now"
+
+            Console.WriteLine("Enter your year of birth:");
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+
+            if (2022 - YearOfBirth < 18)
+            {
+                Console.WriteLine($"You are {2022 - YearOfBirth} years old, You are too young to drive!");
+            }
+            else if (2022 - YearOfBirth > 18)
+            {
+                Console.WriteLine($"You are {2022 - YearOfBirth} years old, Drive carefully!");
+            }
+            else
+            {
+                Console.WriteLine($"You are {2022 - YearOfBirth} years old, Congratulations! You may apply for your driving lisence now.");
+            }
+
+
+
+        }
+    }
+}
---
+++ UserPIN/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UserPIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //приложение просит пользователя ввести PIN-код
+            //Если введенный PIN-код "1234", то приложение приветствует пользователя
+            //*и спрашивает его имя. А затем приветствует пользователя по имени Irina.
+            //В ином случае, консоль выводит на экран сообщение об ошибке Invalid PIN
+
+            Console.WriteLine("Please enter your PIN:");
+            string PIN = Console.ReadLine();
+
+
+            if (PIN == "1234")
+            {
+                Console.WriteLine("Welcome!");
+
+                Console.WriteLine("Please enter your username:");
+                string userName = Console.ReadLine();
+
+                Console.WriteLine("Welcome, Irina!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid PIN.");
+            }
+
+
+        }
+    }
+}
